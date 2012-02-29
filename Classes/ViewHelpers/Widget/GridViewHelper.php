83aa87e7daba3974ec848a635c636dc9bdb9385b
Claus Due <claus@wildside.dk>
2012-02-29 18:16:42 +0100

    [TASK] Prevent Grid Widget from working in anything but BE mode
    The Grid ViewHelper is only intended for BE use.
    
    Change-Id: Id447975406696c7e9ec15f04ae0f887f23cff90c

--- Classes/ViewHelpers/Widget/GridViewHelper.php
+++ Classes/ViewHelpers/Widget/GridViewHelper.php
@@ -47,7 +47,9 @@ class Tx_Flux_ViewHelpers_Widget_GridViewHelper extends Tx_Fluid_Core_Widget_Abs
 	 * @return string
 	 */
 	public function render() {
-		#var_dump($this->templateVariableContainer->get('grid'));
+		if (TYPO3_MODE !== 'BE') {
+			return '';
+		}
 		if ($this->templateVariableContainer->exists('grid')) {
 			$this->controller->setGrid($this->templateVariableContainer->get('grid'));
 		}
