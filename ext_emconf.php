74604f55b8215c23138234600fa0f4f719a444d4
Claus Due <claus@wildside.dk>
2012-10-14 01:18:45 +0200

    [TER] 1.3.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fluidcontent".
 #
-# Auto generated 01-09-2012 18:16
+# Auto generated 14-10-2012 01:16
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '1.3.2',
+	'version' => '1.3.3',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"a6bf";s:14:"ext_tables.php";s:4:"8085";s:34:"Configuration/Common/constants.txt";s:4:"d41d";s:30:"Configuration/Common/setup.txt";s:4:"acb7";s:44:"Configuration/TwitterBootstrap/constants.txt";s:4:"d41d";s:40:"Configuration/TwitterBootstrap/setup.txt";s:4:"f012";s:60:"Resources/Private/Elements/Common/Display/AddressAndMap.html";s:4:"91c0";s:55:"Resources/Private/Elements/Common/Display/FileList.html";s:4:"c000";s:53:"Resources/Private/Elements/Common/Layout/Columns.html";s:4:"1b6c";s:63:"Resources/Private/Elements/Common/Layout/ContentRandomizer.html";s:4:"2aac";s:61:"Resources/Private/Elements/Common/Layout/JQueryAccordion.html";s:4:"a98f";s:56:"Resources/Private/Elements/Common/Layout/JQueryTabs.html";s:4:"5415";s:60:"Resources/Private/Elements/Common/Navigation/BreadCrumb.html";s:4:"8e08";s:58:"Resources/Private/Elements/Common/Navigation/Redirect.html";s:4:"b96c";s:54:"Resources/Private/Elements/Common/Resource/Script.html";s:4:"50af";s:63:"Resources/Private/Elements/Common/Resource/SocialBookmarks.html";s:4:"bc79";s:53:"Resources/Private/Elements/Common/Resource/Style.html";s:4:"f309";s:58:"Resources/Private/Elements/TwitterBootstrap/Accordion.html";s:4:"f8a1";s:54:"Resources/Private/Elements/TwitterBootstrap/Alert.html";s:4:"c3d1";s:60:"Resources/Private/Elements/TwitterBootstrap/ButtonGroup.html";s:4:"2b0a";s:59:"Resources/Private/Elements/TwitterBootstrap/ButtonLink.html";s:4:"c89f";s:57:"Resources/Private/Elements/TwitterBootstrap/FluidRow.html";s:4:"8c42";s:57:"Resources/Private/Elements/TwitterBootstrap/HeroUnit.html";s:4:"49ac";s:63:"Resources/Private/Elements/TwitterBootstrap/NavigationList.html";s:4:"fb32";s:59:"Resources/Private/Elements/TwitterBootstrap/PageHeader.html";s:4:"db43";s:53:"Resources/Private/Elements/TwitterBootstrap/Tabs.html";s:4:"d492";s:59:"Resources/Private/Elements/TwitterBootstrap/Thumbnails.html";s:4:"2c21";s:53:"Resources/Private/Elements/TwitterBootstrap/Well.html";s:4:"18a8";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
+	'_md5_values_when_last_written' => 'a:33:{s:9:"README.md";s:4:"f971";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"a6bf";s:14:"ext_tables.php";s:4:"8085";s:34:"Configuration/Common/constants.txt";s:4:"d41d";s:30:"Configuration/Common/setup.txt";s:4:"acb7";s:44:"Configuration/TwitterBootstrap/constants.txt";s:4:"d41d";s:40:"Configuration/TwitterBootstrap/setup.txt";s:4:"f012";s:60:"Resources/Private/Elements/Common/Display/AddressAndMap.html";s:4:"91c0";s:55:"Resources/Private/Elements/Common/Display/FileList.html";s:4:"c000";s:61:"Resources/Private/Elements/Common/Flux/AjaxContentLoader.html";s:4:"c5a0";s:53:"Resources/Private/Elements/Common/Layout/Columns.html";s:4:"1b6c";s:63:"Resources/Private/Elements/Common/Layout/ContentRandomizer.html";s:4:"2aac";s:61:"Resources/Private/Elements/Common/Layout/JQueryAccordion.html";s:4:"a98f";s:56:"Resources/Private/Elements/Common/Layout/JQueryTabs.html";s:4:"5415";s:60:"Resources/Private/Elements/Common/Navigation/BreadCrumb.html";s:4:"8e08";s:58:"Resources/Private/Elements/Common/Navigation/Redirect.html";s:4:"b96c";s:54:"Resources/Private/Elements/Common/Resource/Script.html";s:4:"50af";s:63:"Resources/Private/Elements/Common/Resource/SocialBookmarks.html";s:4:"bc79";s:53:"Resources/Private/Elements/Common/Resource/Style.html";s:4:"f309";s:58:"Resources/Private/Elements/TwitterBootstrap/Accordion.html";s:4:"f8a1";s:54:"Resources/Private/Elements/TwitterBootstrap/Alert.html";s:4:"c3d1";s:60:"Resources/Private/Elements/TwitterBootstrap/ButtonGroup.html";s:4:"2b0a";s:59:"Resources/Private/Elements/TwitterBootstrap/ButtonLink.html";s:4:"c89f";s:57:"Resources/Private/Elements/TwitterBootstrap/FluidRow.html";s:4:"8c42";s:57:"Resources/Private/Elements/TwitterBootstrap/HeroUnit.html";s:4:"49ac";s:63:"Resources/Private/Elements/TwitterBootstrap/NavigationList.html";s:4:"fb32";s:59:"Resources/Private/Elements/TwitterBootstrap/PageHeader.html";s:4:"db43";s:53:"Resources/Private/Elements/TwitterBootstrap/Tabs.html";s:4:"d492";s:59:"Resources/Private/Elements/TwitterBootstrap/Thumbnails.html";s:4:"2c21";s:53:"Resources/Private/Elements/TwitterBootstrap/Well.html";s:4:"18a8";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
 );
 
 ?>
