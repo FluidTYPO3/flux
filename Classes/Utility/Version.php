de76dbc053ff5329be890e852ecf2c453931c7e8
Claus Due <claus@wildside.dk>
2012-11-13 11:33:30 +0100

    [TASK] Add Version Utility
    Allows shared querying methods for checking versions of TYPO3 and their capabilities.

---
+++ Classes/Utility/Version.php
@@ -0,0 +1,49 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Version utilities
+ *
+ * @author Claus Due, Wildside A/S
+ * @package Flux
+ * @subpackage Utility
+ */
+class Tx_Flux_Utility_Version {
+
+	/**
+	 * check for versions of TYPO3 which do not consistently pass $fieldName
+	 *
+	 * @return boolean
+	 */
+	public static function assertHasFixedFlexFormFieldNamePassing() {
+		$version = explode('.', TYPO3_version);
+		$isRecent4x5 = ($version[0] == 4 && $version[1] == 5 && $version[2] >= 23);
+		$isRecent4x6 = ($version[0] == 4 && $version[1] == 6 && $version[2] >= 16);
+		$isRecent4x7 = ($version[0] == 4 && $version[1] == 7 && $version[2] >= 8);
+		$isAbove4 = ($version[0] > 4);
+		return ($isAbove4 || $isRecent4x5 || $isRecent4x6 || $isRecent4x7);
+	}
+
+}
