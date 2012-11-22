50886e991aae6633057b74a167047fe7ec8721b1
Claus Due <claus@wildside.dk>
2012-11-23 00:21:22 +0100

    [TASK] Add Content plugin CType registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,14 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Content',
+	array(
+		'Content' => 'render',
+	),
+	array(
+	),
+	Tx_Extbase_Utility_Extension::PLUGIN_TYPE_CONTENT_ELEMENT
+);
