d19dd7124ebf14b9127d26c18aea2263eca62305
Claus Due <claus@wildside.dk>
2012-12-13 21:31:44 +0100

    [TASK] Move extra, necessary </div> tag to BE content area ViewHelper

--- Classes/ViewHelpers/Be/ContentElementViewHelper.php
+++ Classes/ViewHelpers/Be/ContentElementViewHelper.php
@@ -37,6 +37,7 @@ class Tx_Flux_ViewHelpers_Be_ContentElementViewHelper extends Tx_Flux_Core_ViewH
 		$record = $this->arguments['row'];
 		$rendered = $dblist->tt_content_drawHeader($record);
 		$rendered .= $dblist->tt_content_drawItem($record);
+		$rendered .= '</div>';
 		return $rendered;
 	}
 
