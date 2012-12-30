d6ca64230540bbd83e2fc6129af55b5cb1fdede8
Claus Due <claus@wildside.dk>
2012-12-31 00:54:00 +0100

    [TER] 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidcontent".
  *
- * Auto generated 16-12-2012 02:31
+ * Auto generated 31-12-2012 00:53
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:42:{s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"6e69";s:14:"ext_tables.php";s:4:"2de2";s:14:"ext_tables.sql";s:4:"3b19";s:9:"README.md";s:4:"1312";s:16:"Classes/Core.php";s:4:"f90b";s:35:"Classes/Backend/ContentSelector.php";s:4:"e871";s:40:"Classes/Controller/ContentController.php";s:4:"75fb";s:49:"Classes/Provider/ContentConfigurationProvider.php";s:4:"ac77";s:40:"Classes/Service/ConfigurationService.php";s:4:"e529";s:34:"Configuration/Common/constants.txt";s:4:"d41d";s:30:"Configuration/Common/setup.txt";s:4:"a787";s:44:"Configuration/TwitterBootstrap/constants.txt";s:4:"d41d";s:40:"Configuration/TwitterBootstrap/setup.txt";s:4:"cc0f";s:34:"Configuration/TypoScript/setup.txt";s:4:"2f15";s:60:"Resources/Private/Elements/Common/Display/AddressAndMap.html";s:4:"91c0";s:55:"Resources/Private/Elements/Common/Display/FileList.html";s:4:"c000";s:61:"Resources/Private/Elements/Common/Flux/AjaxContentLoader.html";s:4:"c5a0";s:53:"Resources/Private/Elements/Common/Layout/Columns.html";s:4:"1b6c";s:63:"Resources/Private/Elements/Common/Layout/ContentRandomizer.html";s:4:"2aac";s:61:"Resources/Private/Elements/Common/Layout/JQueryAccordion.html";s:4:"a98f";s:56:"Resources/Private/Elements/Common/Layout/JQueryTabs.html";s:4:"5415";s:60:"Resources/Private/Elements/Common/Navigation/BreadCrumb.html";s:4:"8e08";s:58:"Resources/Private/Elements/Common/Navigation/Redirect.html";s:4:"b96c";s:54:"Resources/Private/Elements/Common/Resource/Script.html";s:4:"50af";s:63:"Resources/Private/Elements/Common/Resource/SocialBookmarks.html";s:4:"bc79";s:53:"Resources/Private/Elements/Common/Resource/Style.html";s:4:"f309";s:58:"Resources/Private/Elements/TwitterBootstrap/Accordion.html";s:4:"2cb5";s:54:"Resources/Private/Elements/TwitterBootstrap/Alert.html";s:4:"dac8";s:60:"Resources/Private/Elements/TwitterBootstrap/ButtonGroup.html";s:4:"d8cd";s:59:"Resources/Private/Elements/TwitterBootstrap/ButtonLink.html";s:4:"8ac5";s:57:"Resources/Private/Elements/TwitterBootstrap/FluidRow.html";s:4:"0b3f";s:57:"Resources/Private/Elements/TwitterBootstrap/HeroUnit.html";s:4:"d21c";s:63:"Resources/Private/Elements/TwitterBootstrap/NavigationList.html";s:4:"6108";s:59:"Resources/Private/Elements/TwitterBootstrap/PageHeader.html";s:4:"228c";s:53:"Resources/Private/Elements/TwitterBootstrap/Tabs.html";s:4:"92e9";s:59:"Resources/Private/Elements/TwitterBootstrap/Thumbnails.html";s:4:"94dd";s:53:"Resources/Private/Elements/TwitterBootstrap/Well.html";s:4:"ba17";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"060a";s:38:"Resources/Private/Layouts/Content.html";s:4:"da94";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
+	'_md5_values_when_last_written' => 'a:16:{s:16:"ext_autoload.php";s:4:"7e9d";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"6e69";s:14:"ext_tables.php";s:4:"8631";s:14:"ext_tables.sql";s:4:"3b19";s:9:"README.md";s:4:"2c2c";s:16:"Classes/Core.php";s:4:"e681";s:35:"Classes/Backend/ContentSelector.php";s:4:"e871";s:40:"Classes/Controller/ContentController.php";s:4:"75fb";s:49:"Classes/Provider/ContentConfigurationProvider.php";s:4:"ac77";s:40:"Classes/Service/ConfigurationService.php";s:4:"e529";s:34:"Configuration/TypoScript/setup.txt";s:4:"2f15";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"060a";s:38:"Resources/Private/Layouts/Content.html";s:4:"da94";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:33:"Resources/Public/Icons/Plugin.png";s:4:"50ed";}',
 );
 
 ?>
