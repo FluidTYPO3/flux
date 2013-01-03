64dcc353ac03f508eda2599cb9a33c38c82a1382
Claus Due <claus@wildside.dk>
2013-01-03 18:50:31 +0100

    [TASK] Remove notion of colPos listItemsProcFunc
    - there's a feature that's more of a hindrance then a benefit. If any other extension overrides this, we're back to "INVALID VALUE" anyway.

--- ext_tables.php
+++ ext_tables.php
@@ -48,12 +48,6 @@ t3lib_extMgm::addTCAcolumns('tt_content', array(
 
 $TCA['tt_content']['columns']['colPos']['config']['items'][] = array('LLL:EXT:flux/locallang_db.xml:tt_content.tx_flux_container', '-42');
 $TCA['tt_content']['ctrl']['requestUpdate'] .= ',tx_flux_parent';
-if (t3lib_extMgm::isLoaded('gridelements')) {
-	$TCA['tt_content']['columns']['colPos']['config']['itemsProcFunc'] = 'EXT:flux/Classes/Backend/ExtendedColumnPositionListItemsProcessor.php:Tx_Flux_Backend_ExtendedColumnPositionListItemsProcessor->itemsProcFunc';
-} else {
-	$TCA['tt_content']['columns']['colPos']['config']['itemsProcFunc'] = 'EXT:flux/Classes/Backend/StandaloneColumnPositionListItemsProcessor.php:Tx_Flux_Backend_StandaloneColumnPositionListItemsProcessor->itemsProcFunc';
-}
-
 t3lib_extMgm::addToAllTCAtypes('tt_content', 'tx_flux_parent, tx_flux_column');
 
 Tx_Flux_Core::registerConfigurationProvider('Tx_Flux_Provider_Configuration_ContentObjectConfigurationProvider');
