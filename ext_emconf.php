bf6c46d3e56105d2a9fe0a03a3f525fe639ce7ef
Claus Due <claus@wildside.dk>
2013-01-14 00:26:49 +0100

    [TER] 2.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidcontent".
  *
- * Auto generated 09-01-2013 01:21
+ * Auto generated 14-01-2013 00:26
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.1.1',
+	'version' => '2.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:16:"ext_autoload.php";s:4:"7e9d";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"6e69";s:14:"ext_tables.php";s:4:"8631";s:14:"ext_tables.sql";s:4:"3b19";s:9:"README.md";s:4:"2c2c";s:16:"Classes/Core.php";s:4:"11dc";s:35:"Classes/Backend/ContentSelector.php";s:4:"e871";s:40:"Classes/Controller/ContentController.php";s:4:"9393";s:49:"Classes/Provider/ContentConfigurationProvider.php";s:4:"ac77";s:40:"Classes/Service/ConfigurationService.php";s:4:"e529";s:34:"Configuration/TypoScript/setup.txt";s:4:"2f15";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"060a";s:38:"Resources/Private/Layouts/Content.html";s:4:"da94";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:33:"Resources/Public/Icons/Plugin.png";s:4:"50ed";}',
+	'_md5_values_when_last_written' => 'a:15:{s:16:"ext_autoload.php";s:4:"7e9d";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"6e69";s:14:"ext_tables.php";s:4:"d4d2";s:14:"ext_tables.sql";s:4:"3b19";s:9:"README.md";s:4:"2c2c";s:35:"Classes/Backend/ContentSelector.php";s:4:"e871";s:40:"Classes/Controller/ContentController.php";s:4:"9393";s:49:"Classes/Provider/ContentConfigurationProvider.php";s:4:"0ac5";s:40:"Classes/Service/ConfigurationService.php";s:4:"8a5e";s:34:"Configuration/TypoScript/setup.txt";s:4:"2f15";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"060a";s:38:"Resources/Private/Layouts/Content.html";s:4:"da94";s:34:"Resources/Private/Layouts/FCE.html";s:4:"5a0f";s:33:"Resources/Public/Icons/Plugin.png";s:4:"50ed";}',
 );
 
 ?>
