0d157c635d067e1f4f10dbe8071e87f9b9197c65
Claus Due <claus@wildside.dk>
2013-04-20 01:27:01 +0200

    [BUGFIX] Consider recursive array of values in cache key

--- Classes/Service/FluxService.php
+++ Classes/Service/FluxService.php
@@ -144,7 +144,7 @@ class Tx_Flux_Service_FluxService implements t3lib_Singleton {
 	 * @return mixed
 	 */
 	public function getStoredVariable($templatePathAndFilename, $variableName, $section = 'Configuration', $paths = array(), $extensionName = NULL, $variables = array()) {
-		$variableCheck = array_keys($variables);
+		$variableCheck = serialize($variables);
 		$cacheKey = md5($templatePathAndFilename . $variableName . $extensionName . implode('', $paths) . $section . $variableCheck);
 		if (TRUE === isset(self::$cache[$cacheKey])) {
 			return self::$cache[$cacheKey];
