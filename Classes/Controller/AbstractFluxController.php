f2f278279145edb4af473d1b5a1b23475e421b07
Claus Due <claus@wildside.dk>
2013-05-01 22:25:30 +0200

    [BUGFIX] Assign values of fields not prefixed with "settings"

--- Classes/Controller/AbstractFluxController.php
+++ Classes/Controller/AbstractFluxController.php
@@ -138,6 +138,7 @@ class Tx_Flux_Controller_AbstractFluxController extends Tx_Extbase_MVC_Controlle
 				// as well as the internal $this->settings array as per expected Extbase behavior.
 				$this->settings = t3lib_div::array_merge_recursive_overrule($this->settings, $this->data['settings'], FALSE, TRUE);
 			}
+			$this->view->assignMultiple($this->data);
 			$this->view->assign('settings', $this->settings);
 			$templatePathAndFilename = $this->provider->getTemplatePathAndFilename($row);
 			if (FALSE === file_exists($templatePathAndFilename)) {
