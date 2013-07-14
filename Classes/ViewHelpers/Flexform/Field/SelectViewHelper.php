cb4105fadf3352e92b5c044b480decdb8d25d891
Claus Due <claus@wildside.dk>
2013-07-14 21:33:04 +0200

    [TASK] Switch Select ViewHelper to FormComponent

--- Classes/ViewHelpers/Flexform/Field/SelectViewHelper.php
+++ Classes/ViewHelpers/Flexform/Field/SelectViewHelper.php
@@ -29,7 +29,7 @@
  * @package Flux
  * @subpackage ViewHelpers/Flexform/Field
  */
-class Tx_Flux_ViewHelpers_Flexform_Field_SelectViewHelper extends Tx_Flux_ViewHelpers_Flexform_Field_AbstractFieldViewHelper {
+class Tx_Flux_ViewHelpers_Flexform_Field_SelectViewHelper extends Tx_Flux_ViewHelpers_Flexform_Field_AbstractMultiValueFieldViewHelper {
 
 	/**
 	 * Initialize
@@ -37,74 +37,14 @@ class Tx_Flux_ViewHelpers_Flexform_Field_SelectViewHelper extends Tx_Flux_ViewHe
 	 */
 	public function initializeArguments() {
 		parent::initializeArguments();
-		$this->registerArgument('validate', 'string', 'FlexForm-type validation configuration for this input', FALSE, 'trim');
-		$this->registerArgument('items', 'array', 'Items for the selector multidimensional, matching FlexForm/TCA', FALSE, array());
-		$this->registerArgument('commaSeparatedItems', 'string', 'CSV list of item values which are both labels and values', FALSE);
-		$this->registerArgument('size', 'integer', 'Size of the selector box', FALSE, 1);
-		$this->registerArgument('multiple', 'boolean', 'If TRUE, allows multiple selections', FALSE, FALSE);
-		$this->registerArgument('minItems', 'integer', 'Minimum required number of items to be selected', FALSE, 0);
-		$this->registerArgument('maxItems', 'integer', 'Maxium allowed number of items to be selected', FALSE, 1);
-		$this->registerArgument('table', 'string', 'Define foreign table name to turn selector into a record selector for that table', FALSE, NULL);
-		$this->registerArgument('condition', 'string', 'Condition to use when selecting from "foreignTable", supports FlexForm "foregin_table_where" markers', FALSE, NULL);
-		$this->registerArgument('mm', 'string', 'Optional name of MM table to use for record selection', FALSE, NULL);
-		$this->registerArgument('showThumbs', 'boolean', 'If TRUE, adds thumbnail display when editing in BE', FALSE, TRUE);
-		$this->registerArgument('itemsProcFunc', 'string', 'Optional class name of data provider to fill select options');
-		$this->registerArgument('suggest', 'boolean', 'Add "suggest" box to search for entries', FALSE, FALSE);
-		$this->registerArgument('renderMode', 'string', 'Optional alternative rendering mode, for example "checkbox" or "singlebox" ' .
-			'- a full list of supported values and their impact can be found in the TCA documentation for TYPO3', FALSE, 'select');
-		$this->registerArgument('itemListStyle', 'string', 'Overrides the default list style when maxItems > 1', FALSE, NULL);
-		$this->registerArgument('selectedListStyle', 'string', 'Overrides the default selected list style when maxItems > 1 and renderMode is default', FALSE, NULL);
+		$this->registerArgument('items', 'mixed', 'Items for the selector; array / CSV / Traversable / Query supported', TRUE);
 	}
 
-	/**
-	 * @return array
-	 */
-	public function renderConfiguration() {
-		$fieldConfiguration = $this->getFieldConfig();
-		if (is_array($fieldConfiguration['items']) === TRUE) {
-			$items = array();
-			foreach ($fieldConfiguration['items'] as $key => $item) {
-				if (is_array($item) === TRUE) {
-					$item = array_reverse($item);
-				} else {
-					$item = array($item, $key);
-				}
-				array_push($items, $item);
-			}
-			$fieldConfiguration['items'] = $items;
-		}
-		return $fieldConfiguration;
-	}
-
-	/**
-	 * @return array
-	 */
-	protected function getFieldConfig() {
-		$config = $this->getBaseConfig();
-		$config['type'] = 'select';
-		if ($this->arguments['commaSeparatedItems']) {
-			$config['items'] = array();
-			$itemNames = t3lib_div::trimExplode(',', $this->arguments['commaSeparatedItems']);
-			foreach ($itemNames as $itemName) {
-				array_push($config['items'], array($itemName, $itemName));
-			}
-		} else {
-			$config['items'] = $this->arguments['items'];
-		}
-		$config['size'] = $this->arguments['size'];
-		$config['minitems'] = $this->arguments['minItems'];
-		$config['maxitems'] = $this->arguments['maxItems'];
-		$config['multiple'] = $this->arguments['multiple'] ? 1 : 0;
-		$config['foreign_table'] = $this->arguments['table'];
-		$config['foreign_table_where'] = $this->arguments['condition'];
-		$config['mm'] = $this->arguments['mm'];
-		$config['show_thumbs'] = $this->getFlexFormBoolean($this->arguments['showThumbs']);
-		$config['itemsProcFunc'] = $this->arguments['itemsProcFunc'];
-		$config['suggest'] = $this->arguments['suggest'];
-		$config['renderMode'] = $this->arguments['renderMode'];
-		$config['itemListStyle'] = $this->arguments['itemListStyle'];
-		$config['selectedListStyle'] = $this->arguments['selectedListStyle'];
-		return $config;
+	public function getComponent() {
+		/** @var Tx_Flux_Form_Field_Select $component */
+		$component = $this->getPreparedComponent('Select');
+		$component->setItems($this->arguments['items']);
+		return $component;
 	}
 
 }
