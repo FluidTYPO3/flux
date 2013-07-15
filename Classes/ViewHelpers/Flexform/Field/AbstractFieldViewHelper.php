ae2b4883723a26609f9848e254526aac8eb56be6
Claus Due <claus@wildside.dk>
2013-07-16 00:29:15 +0200

    [TASK] Pass "clearable" to Component

--- Classes/ViewHelpers/Flexform/Field/AbstractFieldViewHelper.php
+++ Classes/ViewHelpers/Flexform/Field/AbstractFieldViewHelper.php
@@ -69,6 +69,7 @@ abstract class Tx_Flux_ViewHelpers_Flexform_Field_AbstractFieldViewHelper extend
 		$component->setInheritEmpty($this->arguments['inheritEmpty']);
 		$component->setStopInheritance($this->arguments['stopInheritance']);
 		$component->setTransform($this->arguments['transform']);
+		$component->setClearable($this->arguments['clear']);
 		return $component;
 	}
 
