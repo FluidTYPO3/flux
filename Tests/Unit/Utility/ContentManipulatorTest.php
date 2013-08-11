d5dbfda1747a0c5cb00589fb83612bb232235d1a
Claus Due <claus@wildside.dk>
2013-08-11 17:21:16 +0200

    [TASK] Extend tests to cover new code

--- Tests/Unit/Utility/ContentManipulatorTest.php
+++ Tests/Unit/Utility/ContentManipulatorTest.php
@@ -29,4 +29,65 @@
  */
 class Tx_Flux_Utility_ContentManipulatorTest extends Tx_Flux_Tests_AbstractFunctionalTest {
 
+	/**
+	 * @test
+	 */
+	public function canDetectParentElementAreaFromRecord() {
+		$result = Tx_Flux_Utility_ContentManipulator::detectParentElementAreaFromRecord(0);
+		$this->assertNull($result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function canDetectParentUidFromRecord() {
+		$result = Tx_Flux_Utility_ContentManipulator::detectParentUidFromRecord(0);
+		$this->assertIsInteger($result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function affectByRequestParametersReturnsEarlyWithUnrecognisedUrl() {
+		$parameters = array(
+			'returnUrl' => 'some.php?arg=1#hascutoffpointbutnovalues'
+		);
+		$record = Tx_Flux_Tests_Fixtures_Data_Records::$contentRecordIsParentAndHasChildren;
+		$tceMain = t3lib_div::makeInstance('t3lib_TCEmain');
+		$result = Tx_Flux_Utility_ContentManipulator::affectRecordByRequestParameters($record, $parameters, $tceMain);
+		$this->assertFalse($result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function affectByRequestParametersAppliesContentAreaAndParentWithRecognisedUrl() {
+		$parameters = array(
+			'returnUrl' => 'some.php?arg=1#areaname:999999'
+		);
+		$record = Tx_Flux_Tests_Fixtures_Data_Records::$contentRecordIsParentAndHasChildren;
+		$tceMain = t3lib_div::makeInstance('t3lib_TCEmain');
+		$result = Tx_Flux_Utility_ContentManipulator::affectRecordByRequestParameters($record, $parameters, $tceMain);
+		$this->assertTrue($result);
+		$this->assertSame('areaname', $record['tx_flux_column']);
+		$this->assertSame('999999', $record['tx_flux_parent']);
+	}
+
+	/**
+	 * @test
+	 */
+	public function affectByRequestParametersAppliesContentAreaAndParentWithRecognisedUrlRelativeToElement() {
+		$parameters = array(
+			'returnUrl' => 'some.php?arg=1#areaname:999999:-999998'
+		);
+		$record = Tx_Flux_Tests_Fixtures_Data_Records::$contentRecordIsParentAndHasChildren;
+		$oldSorting = $record['sorting'];
+		$tceMain = t3lib_div::makeInstance('t3lib_TCEmain');
+		$result = Tx_Flux_Utility_ContentManipulator::affectRecordByRequestParameters($record, $parameters, $tceMain);
+		$this->assertTrue($result);
+		$this->assertSame('areaname', $record['tx_flux_column']);
+		$this->assertSame('999999', $record['tx_flux_parent']);
+		$this->assertNotSame($oldSorting, $record['sorting']);
+	}
+
 }
