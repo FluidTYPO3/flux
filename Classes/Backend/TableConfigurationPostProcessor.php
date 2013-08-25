91fe8f99d14054e1ddd3a8d121e1d9b79a59d6b3
Claus Due <claus@wildside.dk>
2013-08-25 13:33:51 +0200

    [FEATURE] Support for Model objects in TableConfigurationPostProcessor

--- Classes/Backend/TableConfigurationPostProcessor.php
+++ Classes/Backend/TableConfigurationPostProcessor.php
@@ -38,10 +38,10 @@ class Tx_Flux_Backend_TableConfigurationPostProcessor implements t3lib_extTables
 	 * @var array
 	 */
 	private static $tableTemplate = array(
-		'title'     => NULL,
-		'label'     => NULL,
-		'tstamp'    => 'tstamp',
-		'crdate'    => 'crdate',
+		'title' => NULL,
+		'label' => NULL,
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
 		'dividers2tabs' => TRUE,
 		'enablecolumns' => array(),
@@ -57,40 +57,136 @@ class Tx_Flux_Backend_TableConfigurationPostProcessor implements t3lib_extTables
 		/** @var Tx_Flux_Service_FluxService $fluxService */
 		$fluxService = $objectManager->get('Tx_Flux_Service_FluxService');
 		$fluxService->initializeObject();
+		/** @var Tx_Extbase_Persistence_Mapper_DataMapFactory $dataMapFactory */
+		$dataMapFactory = $objectManager->get('Tx_Extbase_Persistence_Mapper_DataMapFactory');
 		$forms = Tx_Flux_Core::getRegisteredFormsForTables();
+		$models = Tx_Flux_Core::getRegisteredFormsForModelObjectClasses();
 		foreach ($forms as $fullTableName => $form) {
-			$extensionName = $form->getExtensionName();
-			$extensionKey = t3lib_div::camelCaseToLowerCaseUnderscored($extensionName);
-			$tableConfiguration = self::$tableTemplate;
-			$tableConfiguration['title'] = $form->getLabel();
-			$fields = array();
-			foreach ($form->getFields() as $field) {
-				$name = $field->getName();
-				$fields[$name] = array(
-					'label' => $field->getLabel(),
-					'config' => $field->buildConfiguration(),
-					'exclude' => $field->getExclude()
-				);
+			$this->processFormForTable($fullTableName, $form);
+		}
+		foreach ($models as $modelClassName => $form) {
+			$map = $dataMapFactory->buildDataMap($modelClassName);
+			$fullTableName = $map->getTableName();
+			if (NULL === $form) {
+				$form = $this->generateFormInstanceFromClassName($modelClassName, $fullTableName);
 			}
-			reset($fields);
-			$tableConfiguration['label'] = key($fields);
-			$tableConfiguration['iconfile'] = t3lib_extMgm::extRelPath($extensionKey) . $form->getIcon();
-			$showRecordsFieldList = implode(',', array_keys($fields));
-			$GLOBALS['TCA'][$fullTableName] = array(
-				'ctrl' => $tableConfiguration,
-				'interface' => array(
-					'showRecordFieldList' => $showRecordsFieldList
-				),
-				'columns' => $fields,
-				'types' => array(
-					0 => array(
-						'showitem' => $showRecordsFieldList
-					)
-				)
-			);
+			$this->processFormForTable($fullTableName, $form);
 		}
 	}
 
+	/**
+	 * @param string $table
+	 * @param Tx_Flux_Form $form
+	 */
+	protected function processFormForTable($table, Tx_Flux_Form $form) {
+		$extensionName = $form->getExtensionName();
+		$extensionKey = t3lib_div::camelCaseToLowerCaseUnderscored($extensionName);
+		$tableConfiguration = self::$tableTemplate;
+		$tableConfiguration['title'] = $form->getLabel();
+		$fields = array();
+		$labelFields = $form->getOption('labels');
+		$enableColumns = array();
+		foreach ($form->getFields() as $field) {
+			$name = $field->getName();
+			$fields[$name] = array(
+				'label' => $field->getLabel(),
+				'config' => $field->buildConfiguration(),
+				'exclude' => $field->getExclude()
+			);
+		}
+		if (TRUE === $form->getOption('hide')) {
+			$enableColumns['disabled'] = 'hidden';
+		}
+		if (TRUE === $form->getOption('start')) {
+			$enableColumns['start'] = 'starttime';
+		}
+		if (TRUE === $form->getOption('end')) {
+			$enableColumns['end'] = 'endtime';
+		}
+		if (TRUE === $form->getOption('frontendUserGroup')) {
+			$enableColumns['fe_group'] = 'fe_group';
+		}
+		#\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($enableColumns);
+		#exit();
+		$tableConfiguration['iconfile'] = t3lib_extMgm::extRelPath($extensionKey) . $form->getIcon();
+		$showRecordsFieldList = implode(',', array_keys($fields));
+		$GLOBALS['TCA'][$table] = array(
+			'title' => $form->getLabel(),
+			'ctrl' => $tableConfiguration,
+			'interface' => array(
+				'showRecordFieldList' => $showRecordsFieldList
+			),
+			'columns' => $fields,
+			'enableColumns' => $enableColumns,
+			'types' => array(
+				0 => array(
+					'showitem' => $showRecordsFieldList
+				)
+			)
+		);
+		if (TRUE === $form->getOption('delete')) {
+			$GLOBALS['TCA'][$table]['delete'] = 'deleted';
+		}
+		if (NULL === $labelFields) {
+			reset($fields);
+			$GLOBALS['TCA'][$table]['label'] = key($fields);
+		} else {
+			$GLOBALS['TCA'][$table]['label'] = array_shift($labelFields);
+			$GLOBALS['TCA'][$table]['label_alt'] = implode(',', $labelFields);
+		}
+	}
+
+	/**
+	 * @param string $class
+	 * @param string $table
+	 * @return Tx_Flux_Form
+	 */
+	protected function generateFormInstanceFromClassName($class, $table) {
+		$extensionName = $this->getExtensionNameFromModelClassName($class);
+		$values = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/Form', NULL);
+		$labels = array_keys(Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/Label', NULL));
+		$hasVisibilityToggle = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/Hide');
+		$hasDeleteToggle = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/Delete');
+		$hasStartTimeToggle = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/StartTime');
+		$hasEndTimeToggle = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/EndTime');
+		$hasFrontendGroupToggle = Tx_Flux_Utility_Annotation::getAnnotationValueFromClass($class, 'Flux/FrontendUserGroup');
+		$form = Tx_Flux_Form::create();
+		$form->setName($table);
+		$form->setExtensionName($extensionName);
+		$form->setOption('labels', $labels);
+		$form->setOption('delete', $hasDeleteToggle);
+		$form->setOption('hide', $hasVisibilityToggle);
+		$form->setOption('start', $hasStartTimeToggle);
+		$form->setOption('end', $hasEndTimeToggle);
+		$form->setOption('frontendUserGroup', $hasFrontendGroupToggle);
+		foreach ($values as $propertyName => $settings) {
+			$field = $form->createField($settings['type'], $propertyName);
+			foreach ($settings['config'] as $parameter => $value) {
+				Tx_Extbase_Reflection_ObjectAccess::setProperty($field, $parameter, $value);
+			}
+		}
+		return $form;
+	}
+
+	/**
+	 * @param string $class
+	 * @return string
+	 */
+	protected function getExtensionNameFromModelClassName($class) {
+		if (FALSE !== strpos($class, '_')) {
+			$parts = explode('_Domain_Model_', $class);
+			$extensionName = substr($parts[0], 3);
+		} else {
+			$parts = explode('\\', $class);
+			$candidate = array_slice($parts, 0);
+			if (1 === count($candidate)) {
+				$extensionName = array_pop($candidate);
+			} else {
+				$extensionName = implode('.', $candidate);
+			}
+		}
+		return $extensionName;
+	}
 
 
 }
