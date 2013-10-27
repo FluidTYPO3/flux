6a1237034862602280cda9d049990dad39f32d19
Claus Due <claus@namelesscoder.net>
2013-10-28 00:40:31 +0100

    [TASK] Add class alias map

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'Tx_Flux_Provider_AbstractConfigurationProvider' => 'Tx_Flux_Provider_AbstractProvider',
+	'Tx_Flux_Provider_AbstractContentObjectConfigurationProvider' => 'Tx_Flux_Provider_AbstractProvider',
+	'Tx_Flux_Provider_AbstractPluginConfigurationProvider' => 'Tx_Flux_Provider_AbstractProvider',
+	'Tx_Flux_Provider_Fallback_FallbackConfigurationProvider' => 'Tx_Flux_Provider_Provider',
+	'Tx_Flux_Provider_Fallback_FallbackContentObjectConfigurationProvider' => 'Tx_Flux_Provider_Provider',
+	'Tx_Flux_Provider_Fallback_FallbackPluginConfigurationProvider' => 'Tx_Flux_Provider_Provider',
+	'Tx_Flux_Provider_ConfigurationProviderInterface' => 'Tx_Flux_Provider_ProviderInterface',
+	'Tx_Flux_Provider_ContentObjectConfigurationProviderInterface' => 'Tx_Flux_Provider_ProviderInterface',
+);
