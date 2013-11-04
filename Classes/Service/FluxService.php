c2fff8f6f2540879530b0f983dae6f335e7f7dc7
Claus Due <claus@namelesscoder.net>
2013-11-04 16:21:03 +0100

    [BUGFIX] Consider an empty templateRootPath same as no path

--- Classes/Service/FluxService.php
+++ Classes/Service/FluxService.php
@@ -232,7 +232,7 @@ class Tx_Flux_Service_FluxService implements t3lib_Singleton {
 	 */
 	public function getViewConfigurationForExtensionName($extensionName) {
 		$configuration = $this->getTypoScriptSubConfiguration(NULL, 'view', $extensionName);
-		if (FALSE === is_array($configuration) || 0 === count($configuration)) {
+		if (FALSE === is_array($configuration) || 0 === count($configuration) || TRUE === empty($configuration['templateRootPath'])) {
 			$configuration = array(
 				'templateRootPath' => 'EXT:' . $extensionName . '/Resources/Private/Templates',
 				'partialRootPath' => 'EXT:' . $extensionName . '/Resources/Private/Partials',
