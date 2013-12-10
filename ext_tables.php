0f68153ff77157b336926e4288f8aeeb636317f2
Danilo Bürger <danilo.buerger@hmspl.de>
2013-12-10 23:57:41 +0100

    [TASK] Remove legacy code

--- ext_tables.php
+++ ext_tables.php
@@ -5,41 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['flux']['setup'] = unserialize($_EXTCONF);
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('tt_content');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', array(
-		'tx_flux_column' => array (
-			'exclude' => 0,
-			'label' => 'LLL:EXT:flux/Resources/Private/Language/locallang.xml:tt_content.tx_flux_column',
-			'config' => array (
-				'type' => 'select',
-				'default' => '',
-				'itemsProcFunc' => 'EXT:flux/Classes/Backend/AreaListItemsProcessor.php:FluidTYPO3\Flux\Backend\AreaListItemsProcessor->itemsProcFunc'
-			)
-		),
-		'tx_flux_parent' => array (
-			'exclude' => 0,
-			'label' => 'LLL:EXT:flux/Resources/Private/Language/locallang.xml:tt_content.tx_flux_parent',
-			'config' => array (
-				'type' => 'select',
-				'foreign_table' => 'tt_content',
-				'foreign_table_where' => "AND tt_content.CType = 'fed_fce' AND tt_content.pid = '###CURRENT_PID###'",
-				'default' => 0,
-				'size' => 1,
-				'maxitems' => 1,
-			)
-		),
-		'tx_flux_children' => array (
-			'exclude' => 0,
-			'config' => array (
-				'type' => 'inline',
-				'foreign_table' => 'tt_content',
-				'foreign_field' => 'tx_flux_parent',
-				'foreign_sortby' => 'sorting',
-			)
-		),
-	)
-);
-
 $TCA['tt_content']['columns']['colPos']['config']['items'][] = array('LLL:EXT:flux/locallang.xml:tt_content.tx_flux_container', '-42');
 
 \FluidTYPO3\Flux\Core::registerConfigurationProvider('FluidTYPO3\Flux\Provider\ContentProvider');
