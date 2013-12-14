165f44be2b6ee46e4e6cb213186b0e6c130ca6cb
Claus Due <claus@namelesscoder.net>
2013-12-14 22:28:33 +0100

    [TASK] Switch to typo3-ci Travis-Integration repository

--- Build/ImportSchema.sql
+++ Build/ImportSchema.sql
@@ -1,2 +1,3 @@
+INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_lowlevel','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
 ALTER TABLE `tt_content` ADD `tx_flux_column` varchar(255) DEFAULT NULL;
 ALTER TABLE `tt_content` ADD `tx_flux_parent` int(11) DEFAULT NULL;
