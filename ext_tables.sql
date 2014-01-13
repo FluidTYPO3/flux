e4968a1fbd12db189c31e8201bc0dbf0f807bf16
Benjamin Mack <benni@typo3.org>
2014-01-13 18:55:09 +0100

    [TASK] Added indizes for tt_content to speed up frontend rendering

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,5 +4,9 @@
 CREATE TABLE tt_content (
   colPos int(11) DEFAULT '0',
   tx_flux_column varchar(255) DEFAULT NULL,
-  tx_flux_parent int(11) DEFAULT NULL
+  tx_flux_parent int(11) DEFAULT NULL,
+  tx_flux_children int(11) DEFAULT NULL,
+
+  KEY index_fluxcolumn (tx_flux_column(12)),
+  KEY index_fluxparentcolumn (tx_flux_column(12),tx_flux_parent)
 );
