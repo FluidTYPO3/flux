500b9e1e42069e2c36de7bdc540fbd04d496fe08
Benjamin Mack <benni@typo3.org>
2014-01-15 19:28:15 +0100

    [BUGFIX] Only clear cache when "Clear configuration cache" is pressed

--- Classes/Provider/AbstractProvider.php
+++ Classes/Provider/AbstractProvider.php
@@ -662,6 +662,10 @@ class AbstractProvider implements ProviderInterface {
 	 * @return void
 	 */
 	public function clearCacheCommand($command = array()) {
+		// only empty the cache when "clear configuration cache is pressed"
+		if ('temp_cached' !== $command['cacheCmd']) {
+			return;
+		}
 		if (TRUE === isset($command['uid'])) {
 			return;
 		}
