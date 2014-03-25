7a3c6d323ba393a2198d4feb360e3e99b15f7e75
Claus Due <claus@namelesscoder.net>
2014-03-25 19:03:13 +0100

    [TASK] Flush potential previously imported be_users from Build

--- Build/ImportSchema.sql
+++ Build/ImportSchema.sql
@@ -1,3 +1,4 @@
+DELETE FROM be_users WHERE email = '_cli_phpunit@example.com';
 INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_lowlevel','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
 INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_phpunit','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
 
