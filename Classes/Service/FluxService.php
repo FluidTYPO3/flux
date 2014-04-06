e3c7e667f7d3f3082a6dd0ecee568c98195a144d
Claus Due <claus@namelesscoder.net>
2014-04-06 16:30:11 +0200

    [TASK] Cleanup by avoiding unnecessary FormComponent->add

--- Classes/Service/FluxService.php
+++ Classes/Service/FluxService.php
@@ -249,10 +249,10 @@ class FluxService implements SingletonInterface {
 			$this->debug($error);
 			/** @var Form $form */
 			self::$cache[$cacheKey] = $this->objectManager->get('FluidTYPO3\Flux\Form');
-			self::$cache[$cacheKey]->add(self::$cache[$cacheKey]
-				->createField('UserFunction', 'error')
+			self::$cache[$cacheKey]->createField('UserFunction', 'error')
 				->setFunction('FluidTYPO3\Flux\UserFunction\ErrorReporter->renderField')
-				->setArguments(array($error)));
+				->setArguments(array($error)
+			);
 		}
 		return self::$cache[$cacheKey];
 	}
