e8e19fdc20bc3bd4d8f2e46f94281ccb4dd8f255
Claus Due <claus@namelesscoder.net>
2014-04-14 00:20:15 +0200

    [TASK] Fix build script

--- Build/ImportSchema.sql
+++ Build/ImportSchema.sql
@@ -2,8 +2,16 @@ DELETE FROM be_users WHERE email = '_cli_phpunit@example.com';
 INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_lowlevel','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
 INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_phpunit','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
 
-ALTER TABLE sys_category ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
-ALTER TABLE sys_category_record_mm ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
+
+delimiter ;;
+create procedure csduicvbyujdshblcjdkscndklsjhcdsucgds ()
+begin
+    declare continue handler for 1060 begin end;
+    ALTER TABLE sys_category ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
+    ALTER TABLE sys_category_record_mm ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
+end;;
+call csduicvbyujdshblcjdkscndklsjhcdsucgds();;
+
 ALTER TABLE `tt_content` ADD `tx_flux_column` varchar(255) DEFAULT NULL;
 ALTER TABLE `tt_content` ADD `tx_flux_parent` int(11) DEFAULT NULL;
 
@@ -151,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `cf_extbase_typo3dbbackend_tablecolumns_tags` (
   KEY `cache_tag` (`tag`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `cf_extbase_typo3dbbackend_queries` (
+CREATE TABLE IF NOT EXISTS `cf_extbase_typo3dbbackend_queries` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `identifier` varchar(250) NOT NULL DEFAULT '',
   `expires` int(11) unsigned NOT NULL DEFAULT '0',
@@ -160,7 +168,7 @@ CREATE TABLE `cf_extbase_typo3dbbackend_queries` (
   KEY `cache_id` (`identifier`,`expires`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `cf_extbase_typo3dbbackend_queries_tags` (
+CREATE TABLE IF NOT EXISTS `cf_extbase_typo3dbbackend_queries_tags` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `identifier` varchar(250) NOT NULL DEFAULT '',
   `tag` varchar(250) NOT NULL DEFAULT '',
