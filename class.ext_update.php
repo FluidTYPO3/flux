089e55a842f7bacfa08d3264b476602de26a2180
Claus Due <claus@namelesscoder.net>
2014-09-18 04:02:30 +0200

    [TASK] Use EM script for safe reflection cache clearing

--- class.ext_update.php
+++ class.ext_update.php
@@ -19,6 +19,10 @@ class ext_update {
 	 */
 	public function main() {
 		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'colPos = -42', array('colPos' => 18181));
-		return $GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows have been updated';
+		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection');
+		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection_tags');
+		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object');
+		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object_tags');
+		return $GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows have been updated. System object caches cleared.';
 	}
 }
