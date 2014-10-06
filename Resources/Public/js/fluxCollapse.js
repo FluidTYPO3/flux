afaa5c7416363d80cf9ca3d9e695844c26ba6ec9
Cedric Ziel <cedric@cedric-ziel.com>
2014-10-06 14:57:42 +0200

    [TASK] Adjust commas and semicolons

--- Resources/Public/js/fluxCollapse.js
+++ Resources/Public/js/fluxCollapse.js
@@ -28,15 +28,15 @@ Ext.ns('FluidTYPO3', 'FluidTYPO3.Components');
 
 FluidTYPO3.Components.FluxCollapse = {
 
-	init: function() {
+	init: function () {
 		Ext.select('.toggle-content').on('click', this.fluxCollapse, this);
 	},
 
-	fluxCollapse: function(event, target) {
+	fluxCollapse: function (event, target) {
 		var cookie = Ext.decode(Ext.util.Cookies.get('fluxCollapseStates'));
 		if (cookie == '') {
 			cookie = [];
-		};
+		}
 
 		var toggle = Ext.get(target),
 			toggleContent = toggle.findParent('.toggle-content', null, true),
@@ -49,21 +49,19 @@ FluidTYPO3.Components.FluxCollapse = {
 			for (var i in cookie) {
 				if (cookie[i] == uid) {
 					delete(cookie[i]);
-				};
-			};
+				}
+			}
 		} else {
 			fluxGrid.addClass('flux-grid-hidden');
 			toggle.replaceClass('t3-icon-view-table-collapse', 't3-icon-view-table-expand');
 			if (cookie.indexOf(uid) < 0) {
 				cookie.push(uid);
-			};
-		};
-
+			}
+		}
 		Ext.util.Cookies.set('fluxCollapseStates', Ext.encode(cookie));
-	},
+	}
+};
 
-}
-
-Ext.onReady(function() {
+Ext.onReady(function () {
 	FluidTYPO3.Components.FluxCollapse.init();
 });
