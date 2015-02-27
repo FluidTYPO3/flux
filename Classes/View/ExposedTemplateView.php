566d6ba2414e154b93f11aecedff286260ec2359
Tobias Hein <tobias.hein@netresearch.de>
2015-02-27 10:10:39 +0100

    [TASK] Store compiled templates if they are compilable.
    Ask parsed template if it can be compiled - if it can, store it in the compiler to generate a compiled version.
    
    Signed-off-by: Claus Due <claus@namelesscoder.net>

--- Classes/View/ExposedTemplateView.php
+++ Classes/View/ExposedTemplateView.php
@@ -160,6 +160,9 @@ class ExposedTemplateView extends TemplateView implements ViewInterface {
 			$parsedTemplate = $this->templateCompiler->get($templateIdentifier);
 		} else {
 			$parsedTemplate = $this->templateParser->parse($source);
+			if (TRUE === $parsedTemplate->isCompilable()) {
+				$this->templateCompiler->store($templateIdentifier, $parsedTemplate);
+			}
 		}
 		return $parsedTemplate;
 	}
