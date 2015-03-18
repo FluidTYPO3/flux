7895f63761548c8141ebadf8bc1295ad7f3b26f9
Claus Due <claus@namelesscoder.net>
2015-03-18 13:25:03 +0100

    [TASK] Set correct TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.3.99',
+			'typo3' => '6.2.0-7.1.99',
 			'cms' => ''
 		),
 		'conflicts' => array(
