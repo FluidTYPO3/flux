fd45f94a13d3b79a1a42f5afe7d96dca949fbf5e
Alexander Bohndorf <bohndorf@sitegeist.de>
2015-09-14 16:51:56 +0200

    [BUGFIX] Corrected calculation for tx_flux_parent if original record in default language doesn't have a tx_flux_parent

--- Classes/Service/ContentService.php
+++ Classes/Service/ContentService.php
@@ -349,8 +349,11 @@ class ContentService implements SingletonInterface {
 				// look for the translated version of the parent record indicated
 				// in this new, translated record. Below, we adjust the parent UID
 				// so it has the UID of the translated parent if one exists.
-				$translatedParents = (array) $this->workspacesAwareRecordService->get('tt_content', 'uid', "t3_origuid = '" . $oldRecord['tx_flux_parent'] . "'");
-				$translatedParent = reset($translatedParents);
+				$translatedParent = NULL;
+				if(isset($oldRecord['tx_flux_parent']) && $oldRecord['tx_flux_parent']!=='0') {
+					$translatedParents = (array)$this->workspacesAwareRecordService->get('tt_content', 'uid', "t3_origuid = '" . $oldRecord['tx_flux_parent'] . "'");
+					$translatedParent = reset($translatedParents);
+				}
 				$sortbyFieldName = TRUE === isset($GLOBALS['TCA']['tt_content']['ctrl']['sortby']) ?
 					$GLOBALS['TCA']['tt_content']['ctrl']['sortby'] : 'sorting';
 				$overrideValues = array(
