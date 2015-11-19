2076e898e1f45bb8253eeed72d87caf6f250a240
Claus Due <claus@namelesscoder.net>
2015-11-20 00:21:31 +0100

    [TASK] Raise minimum required TYPO3 version to 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.4.0-7.6.99',
+			'typo3' => '7.6.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
