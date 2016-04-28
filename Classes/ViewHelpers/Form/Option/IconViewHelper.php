887de43c8271baea1d6ada7a81303eedd5298658
Christian Weiske <weiske@mogic.com>
2016-04-28 16:14:51 +0200

    [DOC] flux:form.option.icon documentation

--- Classes/ViewHelpers/Form/Option/IconViewHelper.php
+++ Classes/ViewHelpers/Form/Option/IconViewHelper.php
@@ -12,7 +12,20 @@ use FluidTYPO3\Flux\Form;
 use FluidTYPO3\Flux\ViewHelpers\Form\OptionViewHelper;
 
 /**
- * Form icon option ViewHelper
+ * ### Content element icon
+ *
+ * Will be used in the new content element wizard, in the "Fluid Content type"
+ * drop down, as well as in the web -> page as content element icon.
+ *
+ * ``value`` needs to be the absolute path to the image file, e.g.
+ * ``/typo3conf/ext/myext/Resources/Public/Icons/Element.svg``.
+ *
+ * If this option is not given, the standard path is used:
+ * ``Resources/Public/Icons/Content/$name.svg``.
+ *
+ * #### Example
+ *
+ *     <flux:form.option.icon value="/typo3conf/ext/myext/Resources/Public/Icons/Element.svg"/>
  */
 class IconViewHelper extends OptionViewHelper {
 
