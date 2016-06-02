cbbcaa3496164c767e7c4f137784e29a9aa661d6
Claus Due <claus@namelesscoder.net>
2016-06-02 13:55:51 +0200

    [DOC] Pure whitespace fix

--- Classes/ViewHelpers/Wizard/LinkViewHelper.php
+++ Classes/ViewHelpers/Wizard/LinkViewHelper.php
@@ -19,7 +19,7 @@ use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
  *     <flux:field.input name="link">
  *         <flux:wizard.link/>
  *     </flux:field.input>
- * 
+ *
  * See https://docs.typo3.org/typo3cms/TCAReference/AdditionalFeatures/CoreWizardScripts/Index.html
  * for details about the behaviors that are controlled by arguments.
  */
--- Classes/ViewHelpers/Wizard/ListViewHelper.php
+++ Classes/ViewHelpers/Wizard/ListViewHelper.php
@@ -13,7 +13,7 @@ use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
 
 /**
  * Field Wizard: List
- * 
+ *
  * See https://docs.typo3.org/typo3cms/TCAReference/AdditionalFeatures/CoreWizardScripts/Index.html
  * for details about the behaviors that are controlled by arguments.
  */
