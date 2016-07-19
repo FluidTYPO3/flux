3097f0299ee0b047cc88a6757a2ac3dd0147975f
Daniel Neugebauer <dneuge@glutrot.de>
2016-07-19 12:48:42 +0200

    [TASK] Update script resets Flux parent if column is not set
    Due to #1125, content elements may refer to a random record uid
    as tx_flux_parent. All affected records can be identified by an
    empty tx_flux_column. While the cause has been prevented for future
    record translations and a safety check has been added to avoid the
    most serious issue of content disappearing from FE/BE due to
    assignment of COLPOS_FLUXCONTENT, invalid references still exist in
    DB. This commit adds an update query to reset tx_flux_parent to 0
    for all affected records, so the invalid references are permanently
    cleaned up.
    
    Note that this does not restore any content elements which already
    have disappeared from BE/FE, that task needs to be done manually!

--- class.ext_update.php
+++ class.ext_update.php
@@ -19,11 +19,20 @@ class ext_update {
 	 * @return string
 	 */
 	public function main() {
+		$numAffected = 0;
+
 		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'colPos = -42', array('colPos' => 18181));
+		$numAffected += $GLOBALS['TYPO3_DB']->sql_affected_rows();
+
+		// clean up any inconsistencies from issue #1125
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'tx_flux_parent != 0 and ((tx_flux_column is null) or (tx_flux_column = \'\'))', array('tx_flux_parent' => 0));
+		$numAffected += $GLOBALS['TYPO3_DB']->sql_affected_rows();
+
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection_tags');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object_tags');
-		return $GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows have been updated. System object caches cleared.';
+
+		return $numAffected . ' rows have been updated. System object caches cleared.';
 	}
 }
