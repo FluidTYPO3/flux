c7edb451ee241e0987de9e77ebf84030f7d05cf3
Dan Untenzu <untenzu@webit.de>
2017-01-03 13:16:55 +0100

    [FEATURE] Update records with wrong references
    Enable the update script, which may be run in the
    extension manager, to fix records with wrong
    references (wrongly assigned »flux parent«) automatically.
    
    Refs #1176

--- class.ext_update.php
+++ class.ext_update.php
@@ -19,11 +19,23 @@ class ext_update {
 	 * @return string
 	 */
 	public function main() {
+		$content = '';
+
 		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'colPos = -42', array('colPos' => 18181));
+		$content .= 'Switch to positive colPos (see #477): ' .
+			$GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows affected' . PHP_EOL;
+
+		// Fix records with wrong references (see #1176)
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tt_content', 'tx_flux_parent > 0 AND tx_flux_column = \'\'', array('tx_flux_parent' => 0));
+		$content .= 'Fix records with wrong references (see #1176): ' .
+			$GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows affected' . PHP_EOL;
+
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_reflection_tags');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object');
 		$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cf_extbase_object_tags');
-		return $GLOBALS['TYPO3_DB']->sql_affected_rows() . ' rows have been updated. System object caches cleared.';
+		$content .= 'System object caches cleared.' . PHP_EOL;
+
+		return nl2br($content);
 	}
 }
