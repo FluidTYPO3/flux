c9652f65c4c7bee72bfcdf7623ff95ee14d208c1
Claus Due <claus@namelesscoder.net>
2017-01-09 22:36:24 +0100

    [TER] 8.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,48 +1,42 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "flux".
- *
- * Auto generated 18-09-2014 23:56
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Flux: Fluid FlexForms',
-	'description' => 'Backend form and frontend content rendering assistance API with focus on productivity.',
-	'category' => 'misc',
-	'shy' => 0,
-	'version' => '7.4.0',
-	'dependencies' => 'cms',
-	'conflicts' => '',
-	'priority' => 'top',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'FluidTYPO3 Team',
-	'author_email' => 'claus@namelesscoder.net',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-            'typo3' => '7.6.13-8.5.99',
-            'php' => '7.0.0-7.1.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Flux: Fluid FlexForms',
+  'description' => 'Backend form and frontend content rendering assistance API with focus on productivity.',
+  'category' => 'misc',
+  'shy' => 0,
+  'version' => '8.0.0',
+  'dependencies' => 'cms',
+  'conflicts' => '',
+  'priority' => 'top',
+  'loadOrder' => '',
+  'module' => '',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearcacheonload' => 1,
+  'lockType' => '',
+  'author' => 'FluidTYPO3 Team',
+  'author_email' => 'claus@namelesscoder.net',
+  'author_company' => '',
+  'CGLcompliance' => '',
+  'CGLcompliance_note' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.13-8.5.99',
+      'php' => '7.0.0-7.1.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
+  'suggests' => 
+  array (
+  ),
 );
