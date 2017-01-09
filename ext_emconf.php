61e6b9c206ac1e77ef68931205928f5be07cbb2d
Claus Due <claus@namelesscoder.net>
2017-01-09 22:30:28 +0100

    [FEATURE] Set maximum supported TYPO3 version to 8.5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.13-8.4.99',
+            'typo3' => '7.6.13-8.5.99',
             'php' => '7.0.0-7.1.99'
 		),
 		'conflicts' => array(
