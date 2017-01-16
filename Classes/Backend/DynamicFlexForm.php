aee693e7ea63522ba5ff7eba460f0cad08b2a3c6
Claus Due <claus@namelesscoder.net>
2017-01-16 13:53:04 +0100

    [TASK] Corrected doc comment

--- Classes/Backend/DynamicFlexForm.php
+++ Classes/Backend/DynamicFlexForm.php
@@ -276,7 +276,7 @@ class DynamicFlexForm
      * rendering in flex form containers.
      *
      * @param array $dataStructure
-     * @param null $parentIndex
+     * @param null|string $parentIndex
      * @return array
      */
     protected function patchTceformsWrapper(array $dataStructure, $parentIndex = null)
