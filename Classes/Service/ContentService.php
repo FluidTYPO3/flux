f2639d3b129ba3662497130ed1aefdfa18da9e08
monosize <frank.rakow@gmail.com>
2017-02-12 18:08:33 +0100

    [BUGFIX] Pasting a element When pasting an element at a specific position with the paste button, then the element is always pasted at the top of the column. Tested in TYPO3 7.6 and latest TYPO3 8

--- Classes/Service/ContentService.php
+++ Classes/Service/ContentService.php
@@ -241,7 +241,7 @@ class ContentService implements SingletonInterface
                 // sorting value to re-sort after a possibly invalid sorting value is received.
                 list ($pageUid, , $relativeTo, $parentUid, $area, $column) =
                     GeneralUtility::trimExplode('-', $parameters[1]);
-                $sorting = $tceMain->getSortNumber('tt_content', $relativeTo, $pageUid);
+                $sorting = $tceMain->getSortNumber('tt_content', $row['uid'], -$relativeTo);
                 $row['tx_flux_parent'] = $parentUid;
                 $row['tx_flux_column'] = $area;
                 $row['sorting'] = is_array($sorting) ? $sorting['sortNumber'] : $sorting;
