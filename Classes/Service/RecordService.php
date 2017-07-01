29636d9ce201946c5ca2a0264eac9d645f47b499
Claus Due <claus@namelesscoder.net>
2017-07-01 14:05:27 +0200

    [BUGFIX] Fix issue with RecordService->preparedGet field list
    Corrects an attempt to unroll a string field list.

--- Classes/Service/RecordService.php
+++ Classes/Service/RecordService.php
@@ -99,7 +99,7 @@ class RecordService implements SingletonInterface
      */
     public function preparedGet($table, $fields, $condition, $values = [])
     {
-        return $this->getQueryBuilder($table)->select(...$fields)->from($table)->where($condition)->execute()->fetchAll();
+        return $this->getQueryBuilder($table)->select(...explode(',', $fields))->from($table)->where($condition)->execute()->fetchAll();
     }
 
     /**
