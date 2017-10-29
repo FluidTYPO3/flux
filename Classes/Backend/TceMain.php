810a1528d7eb6c75700c82b0382736ebb014f33a
Juan Manuel Vergés Solanas <juanmanuel.vergessolanas@gmail.com>
2017-10-29 23:34:16 +0100

    [REMOVAL] Remove outdated 7.6-specific condition (#1469)
    References: FluidTYPO3/fluidpages#368

--- Classes/Backend/TceMain.php
+++ Classes/Backend/TceMain.php
@@ -779,8 +779,7 @@ class TceMain
             $table,
             't3ver_move_id',
             sprintf(
-                ' %s t3ver_state = 3 AND deleted = 0 AND uid = %d',
-                version_compare(ExtensionManagementUtility::getExtensionVersion('workspaces'), '8.0.0', '<') ? 'AND' : '',
+                't3ver_state = 3 AND deleted = 0 AND uid = %d',
                 $uid
             )
         );
