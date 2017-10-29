bb1bbfa2b51770f75498b7c5854b048e4812db33
Claus Due <claus@namelesscoder.net>
2017-10-29 22:17:39 +0100

    Remove outdated condition

--- Classes/Backend/TceMain.php
+++ Classes/Backend/TceMain.php
@@ -779,9 +779,7 @@ class TceMain
             $table,
             't3ver_move_id',
             sprintf(
-                ' %s t3ver_state = 3 AND deleted = 0 AND uid = %d',
-                (ExtensionManagementUtility::isLoaded('workspaces') && version_compare(ExtensionManagementUtility::getExtensionVersion('workspaces'),
-                        '8.0.0', '<')) ? 'AND' : '',
+                't3ver_state = 3 AND deleted = 0 AND uid = %d',
                 $uid
             )
         );
