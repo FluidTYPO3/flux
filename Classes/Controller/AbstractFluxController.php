18f96973fb8f403fdf304c4456cffcf7792f9c95
Marno <marno@MarnoAndarasAir.fritz.box>
2017-12-06 21:32:06 +0100

    append issue

--- Classes/Controller/AbstractFluxController.php
+++ Classes/Controller/AbstractFluxController.php
@@ -395,6 +395,8 @@ abstract class AbstractFluxController extends ActionController
         $this->request->setControllerExtensionName($extensionName);
         $this->request->setControllerActionName($controllerActionName);
         $potentialControllerInstance = $this->objectManager->get($controllerClassName);
+        $response = $this->objectManager->get(Response::class);
+
         try {
             HookHandler::trigger(
                 HookHandler::CONTROLLER_BEFORE_REQUEST,
@@ -406,7 +408,7 @@ abstract class AbstractFluxController extends ActionController
                     'controllerActionName' => $controllerActionName
                 ]
             );
-            $potentialControllerInstance->processRequest($this->request, $this->response);
+            $potentialControllerInstance->processRequest($this->request,$response);
         } catch (StopActionException $error) {
             // intentionally left blank
         }
@@ -414,13 +416,13 @@ abstract class AbstractFluxController extends ActionController
             HookHandler::CONTROLLER_AFTER_REQUEST,
             [
                 'request' => $this->request,
-                'response' => $this->response,
+                'response' => $response,
                 'extensionName' => $extensionName,
                 'controllerClassName' => $controllerClassName,
                 'controllerActionName' => $controllerActionName
             ]
         );
-        return $this->response->getContent();
+        return $response->getContent();
     }
 
     /**
