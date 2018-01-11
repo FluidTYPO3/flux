7ab0c62a0188a8a17bd398e66ea8f8be26848d6d
Claus Due <claus@namelesscoder.net>
2018-01-11 02:17:23 +0100

    [REMOVAL] Remove outdated 7.6 compatibility in TER manifest
    Prevents accidentally installing on incompatible TYPO3 versions.

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.13-8.7.99',
+      'typo3' => '8.7.0-8.7.99',
       'php' => '7.0.0-7.2.99',
     ),
     'conflicts' => 
