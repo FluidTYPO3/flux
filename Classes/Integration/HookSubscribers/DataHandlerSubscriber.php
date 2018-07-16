ec0cf25f6f4e9de07d8a309e2d5471afe2e70224
Attila Glück <attila.gluck@gmail.com>
2018-07-16 09:16:24 +0200

    [BUGFIX] Add workaround for forge Issue 85013
    Fixes problems where syncronised translated records did not get moved correctly when moving the original record to a different column.

--- Classes/Integration/HookSubscribers/DataHandlerSubscriber.php
+++ Classes/Integration/HookSubscribers/DataHandlerSubscriber.php
@@ -79,13 +79,11 @@ class DataHandlerSubscriber
         }
 
         // TYPO3 issue https://forge.typo3.org/issues/85013 "colPos not part of $fieldArray when dropping in top column".
-        // We catch the special case of a record being moved, but the target pid being "Root" which is the identifying
-        // symptom of this bug.
         // TODO: remove when expected solution, the inclusion of colPos in $fieldArray, is merged and released in TYPO3
-        if (isset($dataHandler->cmdmap[$table][$id]['move']) && $dataHandler->cmdmap[$table][$id]['move'] === 'Root') {
+        if (!array_key_exists('colPos', $fieldArray)) {
             $record = $this->getSingleRecordWithoutRestrictions($table, (int) $id, 'pid, colPos, l18n_parent');
             $uidInDefaultLanguage = $record['l18n_parent'];
-            if ($uidInDefaultLanguage && isset($dataHandler->datamap[$table][$uidInDefaultLanguage]['colPos'])) {
+            if ($uidInDefaultLanguage && isset($dataHandler->datamap[$table][$uidInDefaultLanguage]['colPos']) && isset($dataHandler->cmdmap[$table][$uidInDefaultLanguage]['move'])) {
                 $fieldArray['colPos'] = (int)($dataHandler->datamap[$table][$uidInDefaultLanguage]['colPos'] ?? $record['colPos']);
             }
             // A massive assignment: 1) force target PID for move, 2) force update of PID, 3) update input field array.
