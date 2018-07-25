e1eaceedbbb293c7e5e9516cdd49a00604ae5313
andinger <login@andikessler.de>
2018-07-25 13:21:08 +0200

    [BUGFIX] Increase integer size of colPos SQL column (#1591)
    Core schema's declaration of INT(11) may not fit the larger colPos values.

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,6 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-  tx_flux_migrated_version varchar(11) DEFAULT NULL
+  tx_flux_migrated_version varchar(11) DEFAULT NULL,
+  colPos bigint(20) DEFAULT '0' NOT NULL
 );
