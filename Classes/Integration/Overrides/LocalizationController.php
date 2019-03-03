ae671cc805f16bf43b90edd72cf367c875aef067
Claus Due <claus@namelesscoder.net>
2019-03-03 03:50:32 +0100

    [FEATURE] Allow translating child content with localize button
    Adds an override for the LocalizationController which
    automatically adds an ad-hoc column entry for
    records which are not located directly in a page column
    and which have a colPos value above the 100 limit.
    
    The result is that child content records will be shown
    with the ability to sync each individual child column,
    without including unintended content from page columns.
    
    Close: #1662

---
+++ Classes/Integration/Overrides/LocalizationController.php
@@ -0,0 +1,76 @@
+<?php
+namespace FluidTYPO3\Flux\Integration\Overrides;
+
+/*
+ * This file is part of the FluidTYPO3/Flux project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use FluidTYPO3\Flux\Utility\ColumnNumberUtility;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Http\JsonResponse;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Versioning\VersionState;
+
+class LocalizationController extends \TYPO3\CMS\Backend\Controller\Page\LocalizationController
+{
+    /**
+     * Get a prepared summary of records being translated
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function getRecordLocalizeSummary(ServerRequestInterface $request): ResponseInterface
+    {
+        $params = $request->getQueryParams();
+        if (!isset($params['pageId'], $params['destLanguageId'], $params['languageId'])) {
+            return new JsonResponse(null, 400);
+        }
+
+        $pageId = (int)$params['pageId'];
+        $destLanguageId = (int)$params['destLanguageId'];
+        $languageId = (int)$params['languageId'];
+
+        $records = [];
+        $result = $this->localizationRepository->getRecordsToCopyDatabaseResult(
+            $pageId,
+            $destLanguageId,
+            $languageId,
+            '*'
+        );
+
+        $columns = [
+            'columns' => [],
+            'columnList' => [],
+        ];
+
+        while ($row = $result->fetch()) {
+            BackendUtility::workspaceOL('tt_content', $row, -99, true);
+            if (!$row || VersionState::cast($row['t3ver_state'])->equals(VersionState::DELETE_PLACEHOLDER)) {
+                continue;
+            }
+            $colPos = $row['colPos'];
+            if (!isset($records[$colPos])) {
+                $records[$colPos] = [];
+            }
+            if ($colPos > ColumnNumberUtility::MULTIPLIER && !isset($columns['columns'][$colPos])) {
+                $columns[$colPos] = 'Unknown';
+                array_push($columns['columnList'], $colPos);
+            }
+            $records[$colPos][] = [
+                'icon' => $this->iconFactory->getIconForRecord('tt_content', $row, Icon::SIZE_SMALL)->render(),
+                'title' => $row[$GLOBALS['TCA']['tt_content']['ctrl']['label']],
+                'uid' => $row['uid']
+            ];
+        }
+
+        return (new JsonResponse())->setPayload([
+            'records' => $records,
+            'columns' => $columns,
+        ]);
+    }
+}
