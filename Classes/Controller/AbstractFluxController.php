816b96c911ec08754f417dd4e867cfb218f64256
Claus Due <claus@namelesscoder.net>
2019-12-05 13:30:50 +0100

    Adapt to still call resolveViewObjectName if it exists

--- Classes/Controller/AbstractFluxController.php
+++ Classes/Controller/AbstractFluxController.php
@@ -270,7 +270,8 @@ abstract class AbstractFluxController extends ActionController
      */
     protected function resolveView()
     {
-        return $this->objectManager->get($this->defaultViewObjectName);
+        $viewClassName = (!method_exists($this, 'resolveViewObjectName') ? $this->defaultViewObjectName : ($this->resolveViewObjectName()) ?: $this->defaultViewObjectName);
+        return $this->objectManager->get($viewClassName)
     }
 
     /**
