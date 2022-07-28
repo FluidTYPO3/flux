4f1429c39b0f658a1e50f6cf6e4c4c383b6003e7
Christian Weiske <weiske@mogic.com>
2022-07-28 09:55:42 +0200

    [DOC] How to handle disabled pages with flux:form.data

--- Classes/ViewHelpers/Form/DataViewHelper.php
+++ Classes/ViewHelpers/Form/DataViewHelper.php
@@ -30,6 +30,19 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
  * can use it to access page configuration data:
  *
  *     <flux:form.data table="pages" field="tx_fed_page_flexform" record="{page}" />
+ *
+ * ### Example: Check if page is accessible before loading data
+ *
+ * Data of disabled and deleted pages cannot be loaded with flux:form.data
+ * and lead to an TYPO3FluidFluidCoreViewHelperException.
+ * To prevent this exception, check if the page is accessible by generating
+ * a link to it:
+ *
+ *     <f:if condition="{f:uri.page(pageUid: myUid)}">
+ *         <flux:form.data table="pages" field="tx_fed_page_flexform" uid="{myUid}" as="pageSettings">
+ *             ...
+ *         </flux:form.data>
+ *     </f:if>
  */
 class DataViewHelper extends AbstractViewHelper
 {
