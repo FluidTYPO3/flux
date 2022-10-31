99ba51528eca355439211d86bfd58a97843a1570
Claus Due <claus@namelesscoder.net>
2022-10-31 19:15:31 +0100

    [TASK] Improve tests

---
+++ Tests/Unit/Content/RecordBased/RecordBasedContentTypeDefinitionRepositoryTest.php
@@ -0,0 +1,125 @@
+<?php
+declare(strict_types=1);
+namespace FluidTYPO3\Flux\Test\Unit\Content\RecordBased;
+
+/*
+ * This file is part of the FluidTYPO3/Flux project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use Doctrine\DBAL\Exception\TableNotFoundException;
+use Doctrine\DBAL\Statement;
+use FluidTYPO3\Flux\Content\TypeDefinition\RecordBased\RecordBasedContentTypeDefinition;
+use FluidTYPO3\Flux\Content\TypeDefinition\RecordBased\RecordBasedContentTypeDefinitionRepository;
+use FluidTYPO3\Flux\Tests\Fixtures\Classes\AccessibleExtensionManagementUtility;
+use FluidTYPO3\Flux\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Package\PackageManager;
+
+class RecordBasedContentTypeDefinitionRepositoryTest extends AbstractTestCase
+{
+    protected function setUp(): void
+    {
+        $packageManager = $this->getMockBuilder(PackageManager::class)
+            ->setMethods(['isPackageActive'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $packageManager->method('isPackageActive')->willReturn(true);
+
+        AccessibleExtensionManagementUtility::setPackageManager($packageManager);
+
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        AccessibleExtensionManagementUtility::setPackageManager(null);
+
+        parent::tearDown();
+    }
+
+    public function testReturnsEmptySetOfDefinitionsOnTableNotExists(): void
+    {
+        $queryBuilder = $this->createQueryBuilderMock();
+        $queryBuilder->method('select')->willThrowException(
+            $this->getMockBuilder(TableNotFoundException::class)->disableOriginalConstructor()->getMock()
+        );
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinitionRepository::class)
+            ->setMethods(['createQueryBuilder'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subject->method('createQueryBuilder')->willReturn($queryBuilder);
+
+        self::assertSame([], $subject->fetchContentTypeDefinitions());
+    }
+
+    public function testCreatesDefinitionInstancesFromResultSet(): void
+    {
+        $definitionRecords = [
+            [
+                'content_type' => 'flux_test',
+                'extension_identity' => 'FluidTYPO3.Flux',
+                'icon' => '',
+            ],
+            [
+                'content_type' => 'flux_test2',
+                'extension_identity' => '',
+                'icon' => '',
+            ],
+
+        ];
+
+        $queryBuilder = $this->createQueryBuilderMock();
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinitionRepository::class)
+            ->setMethods(['createQueryBuilder'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subject->method('createQueryBuilder')->willReturn($queryBuilder);
+
+        $queryBuilder->execute()->method('fetchAll')->willReturn($definitionRecords);
+
+        $definitions = $subject->fetchContentTypeDefinitions();
+        self::assertCount(2, $definitions);
+        self::assertInstanceOf(RecordBasedContentTypeDefinition::class, reset($definitions));
+    }
+
+    protected function createQueryBuilderMock(): MockObject
+    {
+        $statement = $this->getMockBuilder(Statement::class)
+            ->setMethods(['fetchAll'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $expressionBuilder = $this->getMockBuilder(ExpressionBuilder::class)
+            ->setMethods(['eq'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $expressionBuilder->method('eq')->willReturn('');
+
+        $queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+            ->setMethods(
+                [
+                    'select',
+                    'from',
+                    'where',
+                    'expr',
+                    'execute',
+                    'createNamedParameter',
+                ]
+            )
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $queryBuilder->method('select')->willReturnSelf();
+        $queryBuilder->method('from')->willReturnSelf();
+        $queryBuilder->method('where')->willReturnSelf();
+        $queryBuilder->method('expr')->willReturn($expressionBuilder);
+        $queryBuilder->method('createNamedParameter')->willReturn('"foo"');
+        $queryBuilder->method('execute')->willReturn($statement);
+
+        return $queryBuilder;
+    }
+}
--- Tests/Unit/Content/RecordBased/RecordBasedContentTypeDefinitionTest.php
+++ Tests/Unit/Content/RecordBased/RecordBasedContentTypeDefinitionTest.php
@@ -11,12 +11,131 @@ namespace FluidTYPO3\Flux\Tests\Unit\Content\RecordBased;
 use FluidTYPO3\Flux\Content\RuntimeDefinedContentProvider;
 use FluidTYPO3\Flux\Content\TypeDefinition\FluidFileBased\DropInContentTypeDefinition;
 use FluidTYPO3\Flux\Content\TypeDefinition\RecordBased\RecordBasedContentTypeDefinition;
+use FluidTYPO3\Flux\Content\TypeDefinition\RecordBased\RecordBasedContentTypeDefinitionRepository;
+use FluidTYPO3\Flux\Form;
 use FluidTYPO3\Flux\Form\Container\Section;
 use FluidTYPO3\Flux\Provider\Provider;
+use FluidTYPO3\Flux\Tests\Fixtures\Classes\AccessibleExtensionManagementUtility;
 use FluidTYPO3\Flux\Tests\Unit\AbstractTestCase;
+use TYPO3\CMS\Core\Package\Package;
+use TYPO3\CMS\Core\Package\PackageManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RecordBasedContentTypeDefinitionTest extends AbstractTestCase
 {
+    private array $dummyContentConfiguration = [
+        [
+            'sheets' => [
+                'lDEF' => [
+                    'sheets' => [
+                        'el' => [
+                            [
+                                'sheet' => [
+                                    'el' => [
+                                        'name' => [
+                                            'vDEF' => 'sheet',
+                                        ],
+                                        'label' => [
+                                            'vDEF' => 'sheet label',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'sheet' => [
+                'lDEF' => [
+                    'fields' => [
+                        'el' => [
+                            [
+                                'input' => [
+                                    'el' => [
+                                        'name' => [
+                                            'vDEF' => 'field',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    private array $dummyGridConfiguration = [
+        [
+            'grid' => [
+                'lDEF' => [
+                    'gridMode' => [
+                        'vDEF' => Section::GRID_MODE_ROWS,
+                    ],
+                    'autoColumns' => [
+                        'vDEF' => 0,
+                    ],
+                    'columns' => [
+                        [
+                            [
+                                'column' => [
+                                    'el' => [
+                                        'name' => [
+                                            'vDEF' => 'column1',
+                                        ],
+                                        'label' => [
+                                            'vDEF' => 'Label 1',
+                                        ],
+                                        'colPos' => [
+                                            'vDEF' => 1,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                            [
+                                'column' => [
+                                    'el' => [
+                                        'name' => [
+                                            'vDEF' => 'column2',
+                                        ],
+                                        'label' => [
+                                            'vDEF' => 'Label 2',
+                                        ],
+                                        'colPos' => [
+                                            'vDEF' => 2,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    public function testReturnsContentTypeDefinitions(): void
+    {
+        $definitions = [
+            $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+                ->disableOriginalConstructor()
+                ->getMock()
+        ];
+
+        $repository = $this->getMockBuilder(RecordBasedContentTypeDefinitionRepository::class)
+            ->setMethods(['fetchContentTypeDefinitions'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repository->method('fetchContentTypeDefinitions')->willReturn($definitions);
+
+        $singletonInstances = GeneralUtility::getSingletonInstances();
+        GeneralUtility::setSingletonInstance(RecordBasedContentTypeDefinitionRepository::class, $repository);
+
+        self::assertSame($definitions, RecordBasedContentTypeDefinition::fetchContentTypes());
+
+        GeneralUtility::resetSingletonInstances($singletonInstances);
+    }
+
     public function testConstructingInstanceFillsExpectedProperties(): void
     {
         $record = [
@@ -39,7 +158,19 @@ class RecordBasedContentTypeDefinitionTest extends AbstractTestCase
             $subject->getProviderClassName(),
             'Provider class name is unexpected value'
         );
-        //self::assertSame('', $subject->getTemplateSource(), 'Generated template source is unexpected value');
+    }
+
+    public function testGetGridReturnsInternalGridIfDefined(): void
+    {
+        $grid = Form\Container\Grid::create();
+
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+            ->setMethods(['dummy'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->setInaccessiblePropertyValue($subject, 'grid', $grid);
+
+        self::assertSame($grid, $subject->getGrid());
     }
 
     public function testGetGridGeneratesGridWithAutoColumnsEnabledAndGridModeRows(): void
@@ -99,54 +230,7 @@ class RecordBasedContentTypeDefinitionTest extends AbstractTestCase
 
     public function testGetGridGeneratesGridWithManualColumnsEnabledAndGridModeRows(): void
     {
-        $gridConfiguration = [
-            [
-                'grid' => [
-                    'lDEF' => [
-                        'gridMode' => [
-                            'vDEF' => Section::GRID_MODE_ROWS,
-                        ],
-                        'autoColumns' => [
-                            'vDEF' => 0,
-                        ],
-                        'columns' => [
-                            [
-                                [
-                                    'column' => [
-                                        'el' => [
-                                            'name' => [
-                                                'vDEF' => 'column1',
-                                            ],
-                                            'label' => [
-                                                'vDEF' => 'Label 1',
-                                            ],
-                                            'colPos' => [
-                                                'vDEF' => 1,
-                                            ],
-                                        ],
-                                    ],
-                                ],
-                                [
-                                    'column' => [
-                                        'el' => [
-                                            'name' => [
-                                                'vDEF' => 'column2',
-                                            ],
-                                            'label' => [
-                                                'vDEF' => 'Label 2',
-                                            ],
-                                            'colPos' => [
-                                                'vDEF' => 2,
-                                            ],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ];
+        $gridConfiguration = $this->dummyGridConfiguration;
 
         $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
             ->setMethods(['getGridConfiguration'])
@@ -160,54 +244,8 @@ class RecordBasedContentTypeDefinitionTest extends AbstractTestCase
 
     public function testGetGridGeneratesGridWithManualColumnsEnabledAndGridModeColumns(): void
     {
-        $gridConfiguration = [
-            [
-                'grid' => [
-                    'lDEF' => [
-                        'gridMode' => [
-                            'vDEF' => Section::GRID_MODE_COLUMNS,
-                        ],
-                        'autoColumns' => [
-                            'vDEF' => 0,
-                        ],
-                        'columns' => [
-                            [
-                                [
-                                    'column' => [
-                                        'el' => [
-                                            'name' => [
-                                                'vDEF' => 'column1',
-                                            ],
-                                            'label' => [
-                                                'vDEF' => 'Label 1',
-                                            ],
-                                            'colPos' => [
-                                                'vDEF' => 1,
-                                            ],
-                                        ],
-                                    ],
-                                ],
-                                [
-                                    'column' => [
-                                        'el' => [
-                                            'name' => [
-                                                'vDEF' => 'column2',
-                                            ],
-                                            'label' => [
-                                                'vDEF' => 'Label 2',
-                                            ],
-                                            'colPos' => [
-                                                'vDEF' => 2,
-                                            ],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ];
+        $gridConfiguration = $this->dummyGridConfiguration;
+        $gridConfiguration[0]['grid']['lDEF']['gridMode']['vDEF'] = Section::GRID_MODE_COLUMNS;
 
         $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
             ->setMethods(['getGridConfiguration'])
@@ -218,5 +256,146 @@ class RecordBasedContentTypeDefinitionTest extends AbstractTestCase
         $output = $subject->getGrid([]);
         self::assertCount(1, $output->getRows());
         self::assertCount(2, $output->getRows()[0]->getColumns());
+    }
+
+    public function testGetTemplateSourceGeneratesSource(): void
+    {
+        $gridConfiguration = $this->dummyGridConfiguration;
+
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+            ->setMethods(['getGridConfiguration'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subject->method('getGridConfiguration')->willReturn($gridConfiguration);
+        $source = $subject->getTemplateSource();
+        $expected = <<<SOURCE
+<div class="flux-grid">
+<div class="flux-grid-row">
+<flux:content.render area="1" />
+</div>
+<div class="flux-grid-row">
+<flux:content.render area="2" />
+</div>
+</div>
+
+SOURCE;
+
+        self::assertSame($expected, $source);
+    }
+
+    public function testGetTemplateSourceReturnsEmptyStringWithoutGrid(): void
+    {
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+            ->setMethods(['getGrid'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subject->method('getGrid')->willReturn(null);
+        $source = $subject->getTemplateSource();
+
+        self::assertSame('', $source);
+    }
+
+    public function testGetTemplateSourceReturnsSourceFromRecord(): void
+    {
+        $record = [
+            'content_type' => 'flux_test',
+            'icon' => '',
+            'extension_identity' => 'FluidTYPO3.Flux',
+            'template_source' => 'foo',
+        ];
+        $subject = new RecordBasedContentTypeDefinition($record);
+        $source = $subject->getTemplateSource();
+
+        self::assertSame('foo', $source);
+    }
+
+    public function testGetTemplatePathAndFilenameReturnsFileFromRecord(): void
+    {
+        $record = [
+            'content_type' => 'flux_test',
+            'icon' => '',
+            'extension_identity' => 'FluidTYPO3.Flux',
+            'template_file' => realpath('Tests/Fixtures/Templates/Content/AbsolutelyMinimal.html'),
+        ];
+        $subject = new RecordBasedContentTypeDefinition($record);
+        $file = $subject->getTemplatePathAndFilename();
+
+        self::assertSame($record['template_file'], $file);
+    }
+
+    public function testGetTemplatePathAndFilenameReturnsProxyWithoutTemplateInRecord(): void
+    {
+        $package = $this->getMockBuilder(Package::class)
+            ->setMethods(['getPackagePath'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $package->method('getPackagePath')->willReturn('./');
+
+        $packageManager = $this->getMockBuilder(PackageManager::class)
+            ->setMethods(['getPackage', 'isPackageActive'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $packageManager->method('isPackageActive')->willReturn(true);
+        $packageManager->method('getPackage')->willReturn($package);
+
+        $record = [
+            'content_type' => 'flux_test',
+            'icon' => '',
+            'extension_identity' => 'FluidTYPO3.Flux',
+            'template_file' => '',
+        ];
+        $subject = new RecordBasedContentTypeDefinition($record);
+
+        AccessibleExtensionManagementUtility::setPackageManager($packageManager);
+
+        $file = $subject->getTemplatePathAndFilename();
+
+        self::assertSame('./Resources/Private/Templates/Content/Proxy.html', $file);
+
+        AccessibleExtensionManagementUtility::setPackageManager(null);
+    }
+
+    public function testGetFormReturnsExpectedFormComposition(): void
+    {
+        $record = [
+            'content_type' => 'flux_test',
+            'icon' => '',
+            'title' => 'Test',
+            'description' => 'Test description',
+            'extension_identity' => 'FluidTYPO3.Flux',
+            'template_file' => '',
+        ];
+
+        $contentConfiguration = $this->dummyContentConfiguration;
+
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+            ->setMethods(['getContentConfiguration'])
+            ->setConstructorArgs([$record])
+            ->getMock();
+        $subject->method('getContentConfiguration')->willReturn($contentConfiguration);
+
+        $form = $subject->getForm();
+
+        self::assertInstanceOf(Form::class, $form);
+        self::assertInstanceOf(Form\Container\Sheet::class, $form->get('sheet'));
+        self::assertCount(1, $form->get('sheet')->getFields());
+    }
+
+    public function testGetSheetNamesAndLabelsReturnsExpectedValue(): void
+    {
+        $contentConfiguration = $this->dummyContentConfiguration;
+
+        $subject = $this->getMockBuilder(RecordBasedContentTypeDefinition::class)
+            ->setMethods(['getContentConfiguration'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $subject->method('getContentConfiguration')->willReturn($contentConfiguration);
+
+        self::assertSame(
+            [
+                'sheet' => 'Sheet: sheet label',
+            ],
+            iterator_to_array($subject->getSheetNamesAndLabels())
+        );
     }
 }
