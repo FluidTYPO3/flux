a4dbe4f20a2170d45d52e1567dff1134f699e1ba
Claus Due <claus@namelesscoder.net>
2023-07-03 14:40:48 +0200

    [BUGFIX] Avoid PHP8-only constructor strategy and union týpes

--- Classes/Integration/FormEngine/SelectOption.php
+++ Classes/Integration/FormEngine/SelectOption.php
@@ -6,8 +6,22 @@ use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 class SelectOption
 {
     private bool $namedKeys;
-    public function __construct(private string $label, private string|int|null $value, private string|null $icon = null)
+    private string $label;
+    private ?string $icon;
+
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * @param string|int|float|null $value
+     */
+    public function __construct(string $label, $value, ?string $icon = null)
     {
+        $this->label = $label;
+        $this->value = $value;
+        $this->icon = $icon;
         $this->namedKeys = version_compare(VersionNumberUtility::getCurrentTypo3Version(), '12.3', '>=');
     }
 
