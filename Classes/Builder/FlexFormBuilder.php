86b442318858f512e1560a4fd2a063c12ef6a075
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2024-09-16 15:48:21 +0200

    [BUGFIX] Make sure to have correct pid (#2176)
    When you add a new page from list view via page (select position) then BeforeFlexFormDataStructureIdentifierInitializedEvent gives us the negative value of the page uid of the sibling to the choosen position. We need the real parent id though (as the documentation implies is always given).

--- Classes/Builder/FlexFormBuilder.php
+++ Classes/Builder/FlexFormBuilder.php
@@ -70,6 +70,14 @@ class FlexFormBuilder
                 // template selection values as part of our DS identifier.
                 // This is NOT necessary if the input record contains an explicitly selected page layout, hence the
                 // added check above before entering this condition block.
+                if ((integer) $record['pid'] < 0) {
+                    // we have uid of sibling, need parent
+                    $record['pid'] = BackendUtility::getRecord(
+                        'pages',
+                        (integer) abs($record['pid']),
+                        'uid'
+                    )['uid'] ?? 0;
+                }
                 $record = array_merge(
                     $record,
                     $this->pageService->getPageTemplateConfiguration($record['pid'], true) ?? []
