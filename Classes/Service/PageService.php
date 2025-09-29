c4d9e29450ee0345115a193552931863496a547d
Claus Due <claus@namelesscoder.net>
2025-09-29 12:48:41 +0200

    [BUGFIX] Guard against removed/changed/empty template reference

--- Classes/Service/PageService.php
+++ Classes/Service/PageService.php
@@ -30,6 +30,7 @@ use TYPO3\CMS\Core\Utility\RootlineUtility;
 use TYPO3\CMS\Fluid\View\TemplatePaths;
 use TYPO3Fluid\Fluid\Component\Error\ChildNotFoundException;
 use TYPO3Fluid\Fluid\View\Exception\InvalidSectionException;
+use TYPO3Fluid\Fluid\View\Exception\InvalidTemplateResourceException;
 use TYPO3Fluid\Fluid\View\ViewInterface;
 
 /**
@@ -281,7 +282,7 @@ class PageService implements SingletonInterface, LoggerAwareInterface
                     $form->setOption(FormOption::TEMPLATE_FILE_RELATIVE, substr($file->getRelativePathname(), 5, -5));
                     $form->setExtensionName($extensionName);
                     $output[$extensionName][$filename] = $form;
-                } catch (InvalidSectionException | ChildNotFoundException $error) {
+                } catch (InvalidSectionException | ChildNotFoundException | InvalidTemplateResourceException $error) {
                     if ($this->logger instanceof LoggerInterface) {
                         $this->logger->log('error', $error->getMessage());
                     }
