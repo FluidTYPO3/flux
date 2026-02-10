af76cd14163faac13ff09aaf590df45d629a33d7
Matthias Vogel <git@kanti.de>
2026-02-10 08:43:59 +0100

    [BUGFIX] Adjust TCA loading order for schemaFactory
    Fix loading order of TCA for schemaFactory in TYPO3 13.

--- Classes/Integration/Configuration/SpooledConfigurationApplicator.php
+++ Classes/Integration/Configuration/SpooledConfigurationApplicator.php
@@ -92,6 +92,12 @@ class SpooledConfigurationApplicator
                 $scopedRequire($fileInfo->getPathname());
             }
         }
+
+        if (class_exists(TcaSchemaFactory::class)) {
+            // fix TYPO3 13+ loading order of TCA for schemaFactory, flux dose its TCA stuff to late, so we rebuild the tcaSchema after flux has done its work.
+            $tcaSchemaFactory = GeneralUtility::makeInstance(TcaSchemaFactory::class);
+            $tcaSchemaFactory->rebuild($GLOBALS['TCA']);
+        }
     }
 
     private function spoolQueuedContentTypeTableConfigurations(array $queue): void
