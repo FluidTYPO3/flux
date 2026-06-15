271097efd381027a13f0b58df6232e3fcd8cf042
Claus Due <claus@namelesscoder.net>
2026-06-15 09:28:49 +0200

    [TASK] Clean up ViewBuilder for compatibility

--- Tests/Unit/Builder/ViewBuilderTest.php
+++ Tests/Unit/Builder/ViewBuilderTest.php
@@ -15,10 +15,11 @@ use FluidTYPO3\Flux\Integration\PreviewView;
 use FluidTYPO3\Flux\Service\TypoScriptService;
 use FluidTYPO3\Flux\Tests\Unit\AbstractTestCase;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\View\ViewInterface;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use TYPO3\CMS\Fluid\View\TemplatePaths;
 use TYPO3\CMS\Fluid\View\TemplateView;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\View\ViewInterface;
 
 class ViewBuilderTest extends AbstractTestCase
 {
@@ -47,26 +48,27 @@ class ViewBuilderTest extends AbstractTestCase
 
     public function testBuildTemplateView(): void
     {
-        $view = $this->getMockBuilder(TemplateView::class)->disableOriginalConstructor()->getMock();
-        GeneralUtility::addInstance(TemplateView::class, $view);
-
         $subject = $this->getMockBuilder(ViewBuilder::class)
             ->onlyMethods(['buildTemplatePaths', 'createViewInstance'])
             ->setConstructorArgs([$this->renderingContextBuilder, $this->requestBuilder, $this->typoScriptService])
             ->getMock();
         $subject->method('buildTemplatePaths')->willReturn(new TemplatePaths());
         $subject->method('createViewInstance')->willReturn(
-            $this->getMockBuilder(TemplateView::class)->disableOriginalConstructor()->getMockForAbstractClass()
+            $this->getMockBuilder(ViewInterface::class)->getMock()
         );
 
         $view = $subject->buildTemplateView('FluidTYPO3.Flux', 'Default', 'default', 'defaut');
-        self::assertInstanceOf(TemplateView::class, $view);
+        self::assertInstanceOf(ViewInterface::class, $view);
     }
 
     public function testBuildPreviewView(): void
     {
-        $view = $this->getMockBuilder(PreviewView::class)->disableOriginalConstructor()->getMock();
-        GeneralUtility::addInstance(PreviewView::class, $view);
+        $singletons = GeneralUtility::getSingletonInstances();
+        $configManager = $this->getMockBuilder(ConfigurationManager::class)->disableOriginalConstructor()->getMock();
+        GeneralUtility::setSingletonInstance(ConfigurationManager::class, $configManager);
+
+        $previewView = $this->getMockBuilder(PreviewView::class)->disableOriginalConstructor()->getMock();
+        GeneralUtility::addInstance(PreviewView::class, $previewView);
 
         $subject = $this->getMockBuilder(ViewBuilder::class)
             ->onlyMethods(['buildTemplatePaths', 'createViewInstance'])
@@ -79,5 +81,6 @@ class ViewBuilderTest extends AbstractTestCase
 
         $view = $subject->buildPreviewView('FluidTYPO3.Flux', 'Default', 'default', 'default');
         self::assertInstanceOf(PreviewView::class, $view);
+        GeneralUtility::resetSingletonInstances($singletons);
     }
 }
